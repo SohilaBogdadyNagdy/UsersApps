e6474ffddd6cf48a3f5ad2a44c43f81ebabde5bc
sohaila <sohila.bogdady2012@gmail.com>
2018-09-29 21:03:31 +0200

    implement post, delete , get API

--- routes/users.js
+++ routes/users.js
@@ -19,12 +19,33 @@ router.post("/", function(req, res, next){
     console.log(data);
     var _instance = new users(data);
 
-     Save the new model instance, passing a callback
     _instance.save(function (err) {
       if (err) return handleError(err);
-      res.status(201).send({"status", "success", "message": "new user created"})
+      res.status(201).send({"status": "success", "message": "new user created"})
     });
 });
 
+/* GET  one user . */
+router.get('/:user_id', function(req, res, next) {
+  console.log("get one user API");
+  var user_id = req.params.user_id;
+  console.log(user_id);
+  users.find( {_id: user_id },function(err, _user){
+      if (err) return handleError(err);
+      res.status(200).send({"users": _user});
+  });
+
+});
+
+router.delete('/:user_id', function(req, res, next) {
+  console.log("delete user API");
+  var user_id = req.params.user_id;
+  console.log(user_id);
+  users.findOneAndRemove( { _id: user_id },function(err, _user){
+      if (err) return handleError(err);
+      res.status(204).send({"status":"success", "message": "user deleted"});
+  });
+
+});
 
 module.exports = router;
