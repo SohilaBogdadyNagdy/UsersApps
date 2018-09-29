026a441e0d37461c1f9dfe66402ff5c441456092
sohaila <sohila.bogdady2012@gmail.com>
2018-09-29 20:47:54 +0200

    implement create new user API

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,26 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("list users API");
-  users = users.find();
-  res.json(users);
+  users.find( function(err, all_users){
+      if (err) return handleError(err);
+      res.status(200).send({"users": all_users});
+  });
+
 });
 
+router.post("/", function(req, res, next){
+
+    console.log("Create new user");
+    var data = req.body;
+    console.log(data);
+    var _instance = new users(data);
+
+     Save the new model instance, passing a callback
+    _instance.save(function (err) {
+      if (err) return handleError(err);
+      res.status(201).send({"status", "success", "message": "new user created"})
+    });
+});
 
 
 module.exports = router;
